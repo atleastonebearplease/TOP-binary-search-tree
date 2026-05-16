81bcf66d7cd25fe156f23213680dd0ade15d043a
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2026-05-16 11:05:42 -0400

    feat: Add buildTree and helpers
    -Also added to constructor as well as a separate function

--- src/balanced-bst.js
+++ src/balanced-bst.js
@@ -1,3 +1,55 @@
-export class BalancedBST {
-    
+export class Tree {
+    constructor(arr) {
+        this.root = this.buildTree(arr);
+    }
+
+    buildTree(arr) {
+        /* 
+        Sort array
+        Remove duplicates in array
+        Call sortedArrayToBST
+        return root node
+        */
+
+        return this.#sortedArrayToBST(arr);
+    }
+
+    #sortedArrayToBST(arr) {
+        return this.#sortedArrayToBSTRecur(arr, 0, arr.length - 1);
+    }
+
+    #sortedArrayToBSTRecur(arr, start, end) {
+        if(start > end) return null;
+
+        let mid = start + Math.floor((end - start) / 2);
+        let root = new Node(arr[mid]);
+
+        //Divide from middle element
+        root.left = this.#sortedArrayToBSTRecur(arr, start, mid - 1);
+        root.right = this.#sortedArrayToBSTRecur(arr, mid + 1, end);
+
+        return root;
+    }
 }
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+export const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
+
+  prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+}
+
+let tree = new Tree([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+prettyPrint(tree.root);
--- src/test/balanced-bst.test.js
+++ src/test/balanced-bst.test.js
@@ -1,7 +1,33 @@
-import { BalancedBST } from "../balanced-bst.js";
+import { Tree, prettyPrint } from "../balanced-bst.js";
 
-describe('Stuff here', () => {
-    it('Exists', () => {
-        expect(true).toBeTruthy();
+describe('Balanced BST', () => {
+    let tree;
+    
+    beforeEach(() => {
+        tree = new Tree([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
+    
+    it('Is defined', () => {
+        expect(Tree).toBeDefined();
+    });
+
+    describe('buildTree()', () => {
+        it('Handles a sorted array', () => {
+            let node = tree.buildTree([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            expect(node.data).toEqual(5);
+        });
     });
 });
+
+
+/* 
+describe('Hash Map', () => {
+    let hashMap;
+
+    beforeEach(() => {
+        hashMap = new HashMap();
+        hashMap.set('Lydia', 'Fiance');
+        hashMap.set("Nicole", "Mother");
+        hashMap.set("David", "Father");
+    });
+*/
